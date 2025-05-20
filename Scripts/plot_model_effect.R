c5876d86d6fcb5637c98dd46325248179ca6ba86
englishgerrard <gerrardenglish@hotmail.com>
2025-05-20 14:53:46 +0100

    issues with brach

--- Scripts/plot_model_effect.R
+++ Scripts/plot_model_effect.R
@@ -63,6 +63,20 @@ ggplot(filter(cont_clone, VI_type == 'Green'), aes(x = days_scaled, y = estimate
   facet_grid(rows = vars(VI), cols = vars(clone), scales = "free_y") +
   theme_bw()+
   theme(aspect.ratio = 1)
+  
+  ggplot(filter(cont_clone, VI_type == 'Pigment'), aes(x = days_scaled, y = estimate, group=clone, fill = clone)) +
+    geom_ribbon(aes(ymin=lower.HPD.85, ymax = upper.HPD.85), alpha = alpha) +
+    geom_ribbon(aes(ymin=lower.HPD.90, ymax = upper.HPD.90), alpha = alpha) +
+    geom_ribbon(aes(ymin=lower.HPD, ymax = upper.HPD.95, colour = clone), alpha = alpha, linetype  = 2) +
+    geom_ribbon(aes(ymin=lower.HPD.85, ymax = upper.HPD.85), alpha = alpha) +
+    geom_ribbon(aes(ymin=lower.HPD.99, ymax = upper.HPD.99), alpha = alpha) +
+    #geom_ribbon(aes(ymin=lower.HPD.99, ymax = upper.HPD.99),alpha = 0.02, fill = 'red') +
+    #geom_point()+
+    geom_line(aes(colour = clone )) + 
+    geom_hline(yintercept = 0) +
+    facet_grid(rows = vars(VI), cols = vars(clone), scales = "free_y") +
+    theme_bw() +
+    theme(aspect.ratio = 1)
 
 ggplot(filter(cont_clone, VI_type == 'Water'), aes(x = days_scaled, y = estimate, group=clone, fill = clone)) +
   geom_ribbon(aes(ymin=lower.HPD.85, ymax = upper.HPD.85), alpha = alpha) +
@@ -77,19 +91,7 @@ ggplot(filter(cont_clone, VI_type == 'Water'), aes(x = days_scaled, y = estimate
   theme_bw()+
   theme(aspect.ratio = 1)
 
-ggplot(filter(cont_clone, VI_type == 'Pigment'), aes(x = days_scaled, y = estimate, group=clone, fill = clone)) +
-  geom_ribbon(aes(ymin=lower.HPD.85, ymax = upper.HPD.85), alpha = alpha) +
-  geom_ribbon(aes(ymin=lower.HPD.90, ymax = upper.HPD.90), alpha = alpha) +
-  geom_ribbon(aes(ymin=lower.HPD, ymax = upper.HPD.95, colour = clone), alpha = alpha, linetype  = 2) +
-  geom_ribbon(aes(ymin=lower.HPD.85, ymax = upper.HPD.85), alpha = alpha) +
-  geom_ribbon(aes(ymin=lower.HPD.99, ymax = upper.HPD.99), alpha = alpha) +
-  #geom_ribbon(aes(ymin=lower.HPD.99, ymax = upper.HPD.99),alpha = 0.02, fill = 'red') +
-  #geom_point()+
-  geom_line(aes(colour = clone )) + 
-  geom_hline(yintercept = 0) +
-  facet_grid(rows = vars(VI), cols = vars(clone), scales = "free_y") +
-  theme_bw() +
-  theme(aspect.ratio = 1)
+
 
 
 
